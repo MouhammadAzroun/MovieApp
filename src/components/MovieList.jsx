4aba4b16752d5045fc780105d651cb587951a4c2
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-10-13 18:13:17 +0200

    madeAMovieLoghtbox

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const MovieList = (props) => {
+  const [lightboxDisplay, setLightboxDisplay] = useState(false);
+  const [movieToDisplay, setMovieToDisplay] = useState(null);
+
+  const openAndSetLightBox = (movie) => {
+    setMovieToDisplay(movie);
+    setLightboxDisplay(true);
+  };
+
+  const closeLightBox = () => {
+    setLightboxDisplay(false);
+  };
+
   return (
     <div className="movie-list">
       {props.movies.map((movie, index) => (
         <div key={index} className="movie-item">
-          <img src={movie.Poster} alt='movie' />
+          <img
+            src={movie.Poster}
+            alt="movie"
+            onClick={() => openAndSetLightBox(movie)}
+          />
         </div>
       ))}
+      {lightboxDisplay ? (
+        <div id="lightBox" onClick={closeLightBox}>
+          <h3 id="lightBox-title">
+            {movieToDisplay.Title}
+            <br />
+            <p id="lightBox-year">( {movieToDisplay.Year} )</p>
+          </h3>
+          <img id="lightBox-img" src={movieToDisplay.Poster} />
+          <button id="lightBox-addToCartButton">Add to cart</button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };

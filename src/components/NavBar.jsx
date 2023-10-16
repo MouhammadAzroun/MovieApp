e70e1713d7ba156919ab3e250ed76a232f0794bd
Viktorjb <115181183+Viktorjb@users.noreply.github.com>
2023-10-16 22:21:55 +0200

    small fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { actions } from "../features/cart"
+
 
 const NavBar = () => {
+
+   //value är innehållet i kundvagnen
+ const value = useSelector( state => state.cart);
+
   return (
     <div id='navBar'>
-    <button id='cartButton'>Cart</button>
+    <button id='cartButton'>Cart ({value.length})</button>
     </div>
   )
 }

9c660a66bcda8e45ea707096d4226061c74fdd8d
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-10-17 21:50:24 +0200

    css,MovieListHeading,NewRealease

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
-import { actions } from "../features/cart"
+import { actions } from "../features/cart" 
 
 
 const NavBar = () => {
---
+++ src/components/NewReleaseList.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const NewReleaseList = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getMoviesByCurrentYear = async (year) => {
+    const url = `http://www.omdbapi.com/?y=2023&apikey=263d22d8`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setMovies(data.Search); // Assuming 'data.Search' contains an array of movies
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMoviesByCurrentYear("2022");
+  }, []);
+
+  return (
+    <div className='new-release-list'>
+      {loading ? (
+        <p>Loading...</p>
+      ) : movies.length === 0 ? (
+        <p>No movies found</p>
+      ) : (
+        movies.map((movie, index) => (
+          <div key={index} className='movie-item'>
+            <img src={movie.Poster} alt="movie" />
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default NewReleaseList;
